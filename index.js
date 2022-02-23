0b60a03f2b99951088a75f1340cd5be59b6bf34e
Titanium <88758453+Titanium2099@users.noreply.github.com>
2022-02-22 18:59:36 -0800

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import initScrollReveal from "https://cdn.jsdelivr.net/gh/Titanium2099/titanium2099.github.io/scripts/scrollReveal.js";
 import initTiltEffect from "https://cdn.jsdelivr.net/gh/Titanium2099/titanium2099.github.io/scripts/tiltAnimation.js";
-import { targetElements, defaultProps } from "./data/scrollRevealConfig";
+import { targetElements, defaultProps } from "https://cdn.jsdelivr.net/gh/Titanium2099/titanium2099.github.io/data/scrollRevealConfig.js";
 
 initScrollReveal(targetElements, defaultProps);
 initTiltEffect();
